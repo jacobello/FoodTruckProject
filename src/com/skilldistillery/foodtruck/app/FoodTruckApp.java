50601524923343a363ee88691d1ef0d0cbd8ade3
Jacob Nguyen <jn@jacobello.com>
2022-03-25 18:31:25 -0600

    todo

--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -1,10 +1,42 @@
 package com.skilldistillery.foodtruck.app;
 
+import com.skilldistillery.foodtruck.entities.FoodTruck;
+
 public class FoodTruckApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		FoodTruckApp app = new FoodTruckApp();
+		System.out.println("*** Welcome ***");
+//	not ID();
+	}
+//	fleet of up to 5 truck,
+	FoodTruck[] foodtruck = new FoodTruck[5];
+	/*
+	create op();{
+	for loop: ask for input of name, ftype,rating
+	"quit" break;	
+}
+
+
+	choice
+	show menu()
+	4 case swithc loop
+	while != 4
+
+
+
+	create void menu()
+
+	create void existingTruck()
+	
+	create void averageRating()
+	
+	create void highestRating()
+	
+	
+*/
 
 	}
 
-}
+
--- src/com/skilldistillery/foodtruck/entities/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -1,5 +1,9 @@
 package com.skilldistillery.foodtruck.entities;
 
 public class FoodTruck {
-
+	private static int nextTruckId;
+	private String name;
+	private String foodType;
+	private int id;
+	private int rating;
 }
