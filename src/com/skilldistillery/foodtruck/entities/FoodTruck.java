f115b5677f90563323e48fcf7592df9368b5d2c6
Jacob Nguyen <jn@jacobello.com>
2022-03-25 18:38:15 -0600

    getter & setter

--- src/com/skilldistillery/foodtruck/entities/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -4,6 +4,12 @@ public class FoodTruck {
 	private static int nextTruckId;
 	private String name;
 	private String foodType;
-	private int id;
+	private int truckId;
 	private int rating;
+	
+	//get & set, constructor, 
+	
+	
+	
+
 }
