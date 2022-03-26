29a327e3b1a86d9dfa7a8afea57ca27df3c9194a
Jacob Nguyen <jn@jacobello.com>
2022-03-25 18:53:26 -0600

    toString

--- src/com/skilldistillery/foodtruck/entities/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -7,9 +7,64 @@ public class FoodTruck {
 	private int truckId;
 	private int rating;
 	
-	//get & set, constructor, 
-	
-	
-	
 
+	public FoodTruck() {
+		
+	}
+	
+	// get & set, constructor,
+
+	public int getNextTruckId() {
+		return nextTruckId;
+	}
+
+	public void setNextTruckId(int nextTruckId) {
+		FoodTruck.nextTruckId = nextTruckId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFoodType() {
+		return foodType;
+	}
+
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+
+	public int getTruckId() {
+		return truckId;
+	}
+
+	public void setTruckId(int truckId) {
+		this.truckId = truckId;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	//create new Truck
+	public FoodTruck(String name, String foodType, int truckId, int rating) {
+		this.name = name;
+		this.foodType = foodType;
+		this.truckId = truckId;
+		this.rating = rating;
+	}
+	//sysout..
+	public String toString() {
+		return "**Truck Name: " + name + "- ID:" + truckId + ", serving: " + foodType + ". Rating: " + rating + "**"; 
+	}
+	
+	
+	
 }
